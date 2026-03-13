095416ec9f94df7fbf5220f0c69ba3956cd3e23b
AmulyaJambavi <amulyajambavi27@gmail.com>
2026-03-13 17:57:18 +0530

    added refresh

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { register, login, logout } from "../controllers/authController";
+import { register, login, logout,refresh } from "../controllers/authController";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/refresh", refresh); 
+
 
 export default router;
